0d4efba4d3c32afcd8acb101798e5ac5faf7aa81
hyeonsook95 <qu3230@gmail.com>
2020-03-15 22:56:53 +0900

    Web 개발을 위한 필수적인 app, users와 core 생성

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    birthdate = models.DateField(blank=True, null=True)

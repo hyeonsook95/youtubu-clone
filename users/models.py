5dd973a94ea1f0aaf7a346fc00723c714ce3f10d
hyeonsook95 <qu3230@gmail.com>
2020-03-16 08:38:30 +0900

    user avatar 오류 고침

--- users/models.py
+++ users/models.py
@@ -20,6 +20,6 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
-    avatar = models.ProcessedImageField(upload_to="avatars", processors=[ResizeToFill(120, 120)], format='JPEG', options={'quality': 60})
+    avatar = ProcessedImageField(upload_to="avatars", processors=[ResizeToFill(120, 120)], format='JPEG', options={'quality': 60})
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
     birthdate = models.DateField(blank=True, null=True)

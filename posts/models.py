fdf651c657c1c86ad6b590b054637996d6a834fa
hyeonsook95 <qu3230@gmail.com>
2020-03-16 06:48:21 +0900

    post thumbnail과 video의 upload 경로를 변경

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from core.models import TimeStampedModel
+from core.mixins import UploadPathMixin
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class Category(TimeStampedModel):
 
@@ -15,7 +18,7 @@ class Category(TimeStampedModel):
         ordering = ("name",)
 
 
-class Post(TimeStampedModel):
+class Post(TimeStampedModel, UploadPathMixin):
 
     """ Post Model 정의 """
 
@@ -23,6 +26,16 @@ class Post(TimeStampedModel):
     category = models.ForeignKey("Category", on_delete=models.SET_NULL, related_name="posts")
     title = models.CharField(max_length=150)
     description = models.TextField(blank=True, null=True)
-    thumbnail = models.ImageField(upload_to="post/thumbnails")
-    video = models.FileField(upload_to="post/videos")
+    thumbnail = ProcessedImageField(upload_to=data_upload_to, processors=[ResizeToFill(295, 165)], format='JPEG', options={'quality': 60})
+    video = models.FileField(upload_to=data_upload_to)
+
+
+    def data_upload_to(instance, filename):
+
+        subname = super().data_upload_to(instance, filename)
+        return f'post/{subname}'
+
+
+
+    
     
